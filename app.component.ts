e3ef410c25a7d249211f6f7678c0c60c91ae7a8c
hschlais <hschlais@gmail.com>
2017-03-29 13:29:09 -0400

    Root component file.

---
+++ app.component.ts
@@ -0,0 +1,12 @@
+// app.component.ts
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <div class="app">
+      Hello world!
+    </div>
+  `
+})
+export class AppComponent {}

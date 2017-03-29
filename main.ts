154b60c9ed05f8f65599b3edc0004dccbe1cce58
hschlais <hschlais@gmail.com>
2017-03-29 13:44:36 -0400

    Bootstrapping AppModule.
    Typically you bootstrap an Angular app by importing your root module into a new file, main.ts. As Angular can be bootstrapped in multiple environments, such as server-side, we also need to import a module specific to the environment we’re bootstrapping in. For browser bootstrapping, we need to import something called the platform-browser-dynamic module:

---
+++ main.ts
@@ -0,0 +1,6 @@
+// main.ts
+import { AppModule } from './app.module';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
